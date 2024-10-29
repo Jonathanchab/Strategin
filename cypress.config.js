637ccdc9a23c8ee73f124a4dd2d2b6cce8361b51
Jonathanchab <jonathanchab@gmail.com>
2024-10-29 14:19:48 +0100

    Générer un rapport

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  video: true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',      
+    overwrite: true,                  
+    html: true,                        
+    json: true,                       
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
   },
 });
+
